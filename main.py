b93dde4a1d133ed82be0fe60bc955554876bca35
ravi1703 <85504162+ravi1703@users.noreply.github.com>
2021-11-10 10:32:03 +0530

    print statement was updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 
 client.connect('broker.hivemq.com',1883)
-print('Broker connected')
+print('Sucessfully Broker Connected')
 
 client.subscribe('kits/ml')
 
